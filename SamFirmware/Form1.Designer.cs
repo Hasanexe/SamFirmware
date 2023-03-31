b84cd81c1a1b64ddd4eaac408a4b83f024e9f3ec
Hasanexe <35911119+Hasanexe@users.noreply.github.com>
2023-03-31 15:51:55 +0300

    Add files via upload

---
+++ SamFirmware/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SamFirmware
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Firmware";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(158, 126);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(243, 251);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(555, 126);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(243, 251);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(446, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(264, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Add Firmware";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(661, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(137, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Create Firmware";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(555, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 427);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ SamFirmware/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SamFirmware
+{
+    public partial class Form1 : Form
+    {
+        /*private string adb = Path.Combine(Path.GetTempPath(), "adb.exe");
+        private string AdbWinApi = Path.Combine(Path.GetTempPath(), "AdbWinApi.dll");
+        private string AdbWinUsbApi = Path.Combine(Path.GetTempPath(), "AdbWinUsbApi.dll");*/
+        public Form1()
+        {
+
+
+            InitializeComponent();
+            if (Directory.Exists("tmp"))
+            {
+                foreach (var file in Directory.GetFiles("tmp"))
+                {
+                    File.Delete(file);
+                }
+
+                foreach (var directory in Directory.GetDirectories("tmp"))
+                {
+                    Directory.Delete(directory, true);
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+
+                if (openFileDialog1.FileName.Contains("img"))
+                    listBox1.Items.Add(openFileDialog1.FileName);
+
+
+                if (Directory.Exists("tmp"))
+                {
+                    foreach (var file in Directory.GetFiles("tmp"))
+                    {
+                        File.Delete(file);
+                    }
+
+                    foreach (var directory in Directory.GetDirectories("tmp"))
+                    {
+                        Directory.Delete(directory, true);
+                    }
+
+                }
+                else
+                {
+                    Directory.CreateDirectory("tmp");
+                }
+
+                Process process = new Process();
+                process.StartInfo.FileName = "tar.exe";
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+                process.EnableRaisingEvents = false;
+
+                process.StartInfo.Arguments = @" --force-local -C tmp -xvf " + "\"" + openFileDialog1.FileName + "\"";
+                process.Start();
+                process.WaitForExit();
+
+                string folder = "tmp";
+                string[] files = Directory.GetFiles(folder);
+
+
+                foreach (string i in files)
+                    listBox1.Items.Add(i.Remove(0, 4));
+
+
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int intCount = listBox1.SelectedItems.Count - 1; intCount >= 0; intCount--)
+            {
+                if (!listBox2.Items.Contains(listBox1.SelectedItems[intCount]))
+                    listBox2.Items.Add(listBox1.SelectedItems[intCount]);
+                listBox1.Items.Remove(listBox1.SelectedItems[intCount]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int intCount = listBox2.SelectedItems.Count - 1; intCount >= 0; intCount--)
+            {
+                listBox1.Items.Add(listBox2.SelectedItems[intCount]);
+                listBox2.Items.Remove(listBox2.SelectedItems[intCount]);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                if (Directory.Exists("tmp"))
+                {
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = "tar.exe";
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+                    process.EnableRaisingEvents = false;
+
+                    process.StartInfo.Arguments = @" --force-local -C tmp -xvf " + "\"" + openFileDialog1.FileName + "\"";
+                    process.Start();
+                    process.WaitForExit();
+
+                    string folder = "tmp";
+                    string[] files = Directory.GetFiles(folder);
+
+
+
+                    foreach (string i in files)
+                    {
+                        if (!listBox1.Items.Contains(i.Remove(0, 4)) && !listBox2.Items.Contains(i.Remove(0, 4)))
+                            listBox1.Items.Add(i.Remove(0, 4));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Open Firmware First");
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            if (name.Length == 0)
+                name = "firmware";
+            name = name + ".tar";
+            string folder = "tmp";
+            string[] files = Directory.GetFiles(folder);
+            Process process = new Process();
+            process.StartInfo.FileName = "tar.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+            process.EnableRaisingEvents = false;
+
+            if (File.Exists(name))
+            {
+                File.Delete(name);
+            }
+
+            foreach (var item in listBox2.Items)
+            {
+                process.StartInfo.Arguments = @" --transform 's/.*\///g' -rf " + name + " tmp/" + item.ToString();
+                process.Start();
+                process.WaitForExit();
+            }
+
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                File.Move(name, folderBrowserDialog1.SelectedPath + "//" + name);
+            }
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
